5811f24a5f40c218ef561c1b7ef40b0811d9a63a
gcc246 <134176333+gcc246@users.noreply.github.com>
2023-05-20 18:35:14 -0700

    another fix please

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 async function retrieve () {
     var url = "https://api.weatherapi.com/v1/forecast.json?key=7bed2218a1ac4b92ba5171814221705&q=Camarillo&days=2&aqi=yes&alerts=no"
     var req = new Request(url);
+    //api key stuff
     
     const response = await fetch(req);
     const weatherData = await response.text();
