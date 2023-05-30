15b76ee3ff4bde0a24e78856348d48076ac996f4
gcc246 <134176333+gcc246@users.noreply.github.com>
2023-05-29 17:44:43 -0700

    takes input for diff locations

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+let locationInput;
+
+
 async function retrieve () {
-    var url = "https://api.weatherapi.com/v1/forecast.json?key=7bed2218a1ac4b92ba5171814221705&q=Camarillo&days=2&aqi=yes&alerts=no"
+    var url = `https://api.weatherapi.com/v1/forecast.json?key=7bed2218a1ac4b92ba5171814221705&q=${locationInput}&days=2&aqi=yes&alerts=no`
     var req = new Request(url);
     //api key stuff
     
@@ -9,48 +12,35 @@ async function retrieve () {
     console.log(weatherDataText);
     // JSON object parsing to javascript object ^
     
-    let humidity = document.getElementById("humidLabel");
     let temperature = document.getElementById("temperatureF");
-    temperature.textContent += weatherDataText.current.temp_f;
-    humidity.textContent += weatherDataText.current.humidity + "%";
-   
-    let visible = false; // control variable
+    temperature.textContent = "Temperature in Fahrenheit: " +  weatherDataText.current.temp_f;
+    
 
-//button function to toggle humid lable visibility
-    document.getElementById("humidityButton").onclick = function(){
-        if(visible == false){
-            humidity.style.visibility = "visible";
-        }
-        else{
-            humidity.style.visibility = "hidden";  
-        }
-        visible = !visible;
-    }
 
 //wind card
-document.getElementById("windSpeed").textContent += weatherDataText.current["wind_mph"] + " mph"
-document.getElementById("windDegree").textContent += weatherDataText.current["wind_degree"] + "°"
-document.getElementById("windDirection").textContent += weatherDataText.current["wind_dir"]
+document.getElementById("windSpeed").textContent = weatherDataText.current["wind_mph"] + " mph"
+document.getElementById("windDegree").textContent = "Wind Degree: " + weatherDataText.current["wind_degree"] + "°"
+document.getElementById("windDirection").textContent = "Wind Direction: " + weatherDataText.current["wind_dir"]
 document.getElementById("windGrad").style = "background-image: linear-gradient(#e5e7e7,#fdfdfd);"
-document.getElementById("maxWind").textContent += weatherDataText.forecast.forecastday[0].day.maxwind_mph + " mph"
-document.getElementById("windGust").textContent += weatherDataText.current["gust_mph"] +" mph"
+document.getElementById("maxWind").textContent ="Max Wind Speed: " + weatherDataText.forecast.forecastday[0].day.maxwind_mph + " mph"
+document.getElementById("windGust").textContent = "Gusts: " + weatherDataText.current["gust_mph"] +" mph"
 
 
 //astro card
-document.getElementById("sunset").textContent += weatherDataText.forecast.forecastday[0].astro.sunset
-document.getElementById("sunrise").textContent += weatherDataText.forecast.forecastday[0].astro.sunrise
-document.getElementById("uvIndex").textContent += weatherDataText.current.uv;
+document.getElementById("sunset").textContent = "Sunset: " + weatherDataText.forecast.forecastday[0].astro.sunset
+document.getElementById("sunrise").textContent = "Sunrise: " +  weatherDataText.forecast.forecastday[0].astro.sunrise
+document.getElementById("uvIndex").textContent = "UV Index: " +  weatherDataText.current.uv;
 document.getElementById("astroGrad").style = "background-image: linear-gradient(#e5e7e7,#fdfdfd);"
 
 
 //tomorrow card
 
 document.getElementById("conditionTmr").textContent = weatherDataText.forecast.forecastday[1].day.condition.text;
-document.getElementById("maxWindTmr").textContent += weatherDataText.forecast.forecastday[1].day.maxwind_mph + " mph"
-document.getElementById("avgTempTmr").textContent += weatherDataText.forecast.forecastday[1].day.avgtemp_f + "°";
-document.getElementById("maxTempTmr").textContent += weatherDataText.forecast.forecastday[1].day.maxtemp_f + "°";
-document.getElementById("minTempTmr").textContent += weatherDataText.forecast.forecastday[1].day.mintemp_f + "°";
-document.getElementById("avgHumid").textContent += weatherDataText.forecast.forecastday[1].day.avghumidity+ "%";
+document.getElementById("maxWindTmr").textContent = "Max Wind: " +  weatherDataText.forecast.forecastday[1].day.maxwind_mph + " mph"
+document.getElementById("avgTempTmr").textContent = "Average Temperature" +  weatherDataText.forecast.forecastday[1].day.avgtemp_f + "°";
+document.getElementById("maxTempTmr").textContent = "Max Temperature: " +  weatherDataText.forecast.forecastday[1].day.maxtemp_f + "°";
+document.getElementById("minTempTmr").textContent ="Min Temperature: " + weatherDataText.forecast.forecastday[1].day.mintemp_f + "°";
+document.getElementById("avgHumid").textContent = "Average Humidity: " +  weatherDataText.forecast.forecastday[1].day.avghumidity+ "%";
 
 
 
@@ -59,26 +49,19 @@ document.getElementById("avgHumid").textContent += weatherDataText.forecast.fore
 
 //aqi card
 let AQI = weatherDataText.current.air_quality["gb-defra-index"];
+document.getElementById("AQIGBIndex").textContent = "GB Defra Index: " +  weatherDataText.current.air_quality["gb-defra-index"];
+document.getElementById("AQIUSIndex").textContent = "US EPA Index: " +  weatherDataText.current.air_quality["us-epa-index"];
 if (AQI <=3){
     document.getElementById("AQICondition").textContent = "Low Air Pollution";
-    document.getElementById("AQIGBIndex").textContent += weatherDataText.current.air_quality["gb-defra-index"];
-    document.getElementById("AQIUSIndex").textContent += weatherDataText.current.air_quality["us-epa-index"];
-
 }
 else if(AQI>=4 && AQI <=6){
     document.getElementById("AQICondition").textContent = "Moderate Air Pollution";
-    document.getElementById("AQIGBIndex").textContent += weatherDataText.current.air_quality["gb-defra-index"];
-    document.getElementById("AQIUSIndex").textContent += weatherDataText.current.air_quality["us-epa-index"];
 }
 else if(AQI>= 7 && AQI<=9){
     document.getElementById("AQICondition").textContent = "High Air Pollution";
-    document.getElementById("AQIGBIndex").textContent += weatherDataText.current.air_quality["gb-defra-index"];
-    document.getElementById("AQIUSIndex").textContent += weatherDataText.current.air_quality["us-epa-index"];
 }
 else{
-    document.getElementById("AQICondition").textContent = "Very High Air Pollution";
-    document.getElementById("AQIGBIndex").textContent += weatherDataText.current.air_quality["gb-defra-index"];
-    document.getElementById("AQIUSIndex").textContent += weatherDataText.current.air_quality["us-epa-index"];
+    document.getElementById("AQICondition").textContent = "Very High Air Pollution";;
 }
 
 let cards = document.querySelectorAll(".card");
@@ -88,4 +71,11 @@ cards.forEach((card) =>{
 });
 
     }
-    retrieve();
+
+
+    const saveInput = () =>{
+        locationInput = document.getElementById("locationInputForm").value;
+        document.getElementById("locationHeader").textContent = locationInput + " Weather";
+        retrieve();
+        document.getElementById("weatherBody").style.visibility = "visible";
+      }
